444ccb229ed530fed94c70cf84bf9ee43190c818
Naveed Hussain <nhussain2@hawk.iit.edu>
2019-11-11 17:33:05 -0600

    add all map features

--- js/map.js
+++ js/map.js
@@ -1,7 +1,28 @@
-var map;
-function initMap(){
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 37.7936, lng:-122.4840},
-    zoom: 15 // typical is usually 8 or 9
+function initMap() {
+  var myLatLng = {lat: 37.7936, lng: -122.4836};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: myLatLng
   });
-}// end initMap function
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    animation: google.maps.Animation.BOUNCE,
+    icon: 'https://image.flaticon.com/icons/svg/181/181545.svg'
+  });
+
+  var contentString = '<h1>Baker Beach</h1><p>One of my favorite beaches in San Francisco. It is great for hiking (Coastal Trail on the cliffside) and has a historical site called Battery Chamberlin built in 1904.</p>';
+
+  var infowindow = new google.maps.InfoWindow({
+     content: contentString
+  });
+
+
+  google.maps.event.addListener(marker, 'mouseover', function() {
+    infowindow.open(map, marker);
+  });
+
+}
+

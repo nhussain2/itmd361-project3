d04e109556acb2e47d92ff766b05490d62e60bce
Naveed Hussain <nhussain2@hawk.iit.edu>
2019-11-11 15:51:57 -0600

    lat long

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 var map;
 function initMap(){
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.0519752, lng: -76.314270999999},
+    center: {lat: 37.7932, lng:-122.4840},
     zoom: 15 // typical is usually 8 or 9
   });
 }// end initMap function

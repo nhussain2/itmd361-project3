38c6e2c41199abb05ac9d1936f0351794c011eb3
Naveed Hussain <nhussain2@hawk.iit.edu>
2019-11-11 17:02:27 -0600

    fix header

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 var map;
 function initMap(){
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.0519752, lng:-122.4840},
+    center: {lat: 37.7936, lng:-122.4840},
     zoom: 15 // typical is usually 8 or 9
   });
 }// end initMap function
